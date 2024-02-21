2db2a00a4dfc20a9b192249715ba63d15a32b7b5
alxmart <alxmartinez@gmail.com>
2024-02-21 10:58:52 -0300

    Added a new activity (DetalhesActivity)

---
+++ app/src/main/java/com/luizafmartinez/m13_activityfragment/DetalhesActivity.kt
@@ -0,0 +1,12 @@
+
+package com.luizafmartinez.m13_activityfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetalhesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes)
+    }
+}
