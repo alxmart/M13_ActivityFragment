b62ea9d4adde414d7f39595899e63497272bd8c2
alxmart <alxmartinez@gmail.com>
2024-02-21 10:34:57 -0300

    Project created.

---
+++ app/src/main/java/com/luizafmartinez/m13_activityfragment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.luizafmartinez.m13_activityfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
