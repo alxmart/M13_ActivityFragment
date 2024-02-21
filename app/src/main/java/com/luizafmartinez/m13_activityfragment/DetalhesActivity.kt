adbb858a18b4cd69d6bef11723d8f84ed9f4db66
alxmart <alxmartinez@gmail.com>
2024-02-21 11:41:08 -0300

    Code to open and close DetalhesActivity

--- app/src/main/java/com/luizafmartinez/m13_activityfragment/DetalhesActivity.kt
+++ app/src/main/java/com/luizafmartinez/m13_activityfragment/DetalhesActivity.kt
@@ -3,10 +3,21 @@ package com.luizafmartinez.m13_activityfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DetalhesActivity : AppCompatActivity() {
+
+    lateinit var btnFechar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes)
+
+        btnFechar = findViewById(R.id.btn_fechar)
+
+        btnFechar.setOnClickListener {
+            finish()
+        }
+
     }
 }
