7587417e5a812803e5337576eca1247514c89d8d
alxmart <alxmartinez@gmail.com>
2024-02-21 14:20:02 -0300

    Testing activity lifecycle sending status to Log.

--- app/src/main/java/com/luizafmartinez/m13_activityfragment/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/m13_activityfragment/MainActivity.kt
@@ -3,6 +3,7 @@ package com.luizafmartinez.m13_activityfragment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.i("Ciclo_De_Vida", "onCreate")
+
         setContentView(R.layout.activity_main)
 
         btnAbrir = findViewById(R.id.btn_abrir)
@@ -24,8 +28,33 @@ class MainActivity : AppCompatActivity() {
 
             startActivity( intent )
         }
-
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("Ciclo_Vida", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Ciclo_Vida", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Ciclo_Vida", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Ciclo_Vida", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Ciclo_Vida", "onDestroy")
+    }
+
 
 }
 
